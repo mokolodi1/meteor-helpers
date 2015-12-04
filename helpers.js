8f5f60a1f6f090bc1ff275f1d0b51a301bed3bb8
mokolodi1 <mokolodi1@gmail.com>
2015-12-03 18:31:37 -0800

    first commit

---
+++ helpers.js
@@ -0,0 +1,34 @@
+function onlyHash (obj) {
+  return JSON.stringify(obj.hash) === '{}';
+}
+
+Template.registerHelper("print", function (first, second, third) {
+  console.log("first, second, third:", first, second, third);
+  if (first === undefined) {
+    console.log("No arguments given to global print helper :(");
+  } else if (second === undefined) {
+    if (onlyHash(third)) {
+      console.log(first, second);
+    } else {
+      console.log(first);
+    }
+  } else if (third === undefined) {
+    if (onlyHash(second)) {
+      console.log(first);
+    } else {
+      console.log(first, second);
+    }
+  } else if (onlyHash(third)) {
+    console.log(first, second);
+  } else {
+    console.log("current print helper only does 2 arguments...");
+  }
+});
+
+Template.registerHelper('compare', function (first, second) {
+  if (typeof first === 'object' && typeof second === 'object') {
+    return _.isEqual(first, second); // do a object comparison
+  } else {
+    return first === second;
+  }
+});
---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  name: 'mokolodi1:helpers',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'General template and code helpers',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.3');
+
+  api.use('templating', 'client');
+
+  api.export("Template", 'client');
+
+  api.addFiles('helpers.js', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('mokolodi1:helpers');
+  api.addFiles('helpers-tests.js');
+});
