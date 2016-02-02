ac2cd1216db1e95dcb27f58c8974ccb9c270e074
mokolodi1 <mokolodi1@gmail.com>
2016-02-01 19:24:21 -0800

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "mokolodi1:helpers",
-  version: "0.0.8",
+  version: "0.0.9",
   // Brief, one-line summary of the package.
   summary: "General template and code helpers",
   // URL to the Git repository containing the source code for this package.
